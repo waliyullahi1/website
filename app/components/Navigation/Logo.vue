a140f1600a4f9bc822f5d2ab58f4d7d9290b3e9c
Your Name <youremail@example.com>
2025-11-15 19:21:03 +0100

    refactor: implement logo button with project name

--- app/components/Navigation/Logo.vue
+++ app/components/Navigation/Logo.vue
@@ -1,3 +1,10 @@
 <template>
-  
+  <button @click="$emit('logoClicked')" class="flex items-center gap-3 text-4xl text-secondary marcellus">
+    {{ projectName }}
+  </button>
 </template>
+
+<script setup>
+const config = useRuntimeConfig()
+const projectName = config.public.projectName
+</script>
