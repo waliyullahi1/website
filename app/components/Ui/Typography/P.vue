7a2018b15dc87bd7d944d3f34e4b6097128e3ed1
Your Name <youremail@example.com>
2025-11-18 18:32:10 +0100

    refactor: update typography class for responsive text sizes

--- app/components/Ui/Typography/P.vue
+++ app/components/Ui/Typography/P.vue
@@ -1,5 +1,5 @@
 <template>
-  <p :class="[color==='secondary' ? 'text-secondary' : color==='white' ? 'text-white' : color === 'primary' ? 'text-primary': 'text-neutral-800',  small_text ? 'text-sm 2xl:text-base' : 'text-base 2xl:text-lg']" class="font-normal">
+  <p :class="[color==='secondary' ? 'text-secondary' : color==='white' ? 'text-white' : color === 'primary' ? 'text-primary': 'text-neutral-800',  small_text ? 'text-sm lg:text-base' : 'text-base lg:text-lg']" class="font-normal">
     <slot/>
   </p>
 </template>
