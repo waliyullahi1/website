e31fefb46b55a9478cd1cd24b10c49cbee1b11b5
Your Name <youremail@example.com>
2025-11-23 18:51:00 +0100

    feat: add Tawk.to live chat script to app.vue

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,16 @@
   <NuxtPage />
   <NavigationFooter />
 </template>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/69233cd6025e0c19618d54d1/1jaoqbb7e';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
